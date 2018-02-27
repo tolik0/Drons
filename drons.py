e4a796740fa2ce468df1fcd3da5f88296d1c906f
tolik0 <iatsuk@ucu.edu.ua>
2018-02-27 10:42:36 +0200

    Update

--- drons.py
+++ drons.py
@@ -39,24 +39,27 @@ class MagicDron():
 class Battlefield():
 
     def play(self, d1, d2):
-        print("First drone - {} health {}\nSecond drone - {} health {}"
-              .format(type(d1).__name__, d1.health, type(d2).__name__,
-                      d2.health))
-        print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
-        while True:
-            d2.attack(d1)
+        if type(d1) == type(d2) == CivilDron:
+            print("Draw")
+        else:
+            print("First drone - {} health {}\nSecond drone - {} health {}"
+                  .format(type(d1).__name__, d1.health, type(d2).__name__,
+                          d2.health))
             print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
-            if d1.health <= 0:
-                print("Second Drone win")
-                break
-            d1.attack(d2)
-            print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
-            if d2.health <= 0:
-                print("First Drone win")
-                break
+            while True:
+                d2.attack(d1)
+                print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
+                if d1.health <= 0:
+                    print("Second Drone win")
+                    break
+                d1.attack(d2)
+                print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
+                if d2.health <= 0:
+                    print("First Drone win")
+                    break
 
 
 field = Battlefield()
-d1 = ArmoredDron()
+d1 = MagicDron()
 d2 = ArmyDron()
 field.play(d1, d2)
