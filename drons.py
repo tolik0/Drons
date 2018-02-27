babbc7db983dd03ed42461935fa36f30e496eaaf
tolik0 <iatsuk@ucu.edu.ua>
2018-02-27 10:35:31 +0200

    Update

---
+++ drons.py
@@ -0,0 +1,62 @@
+import random
+
+
+class CivilDron():
+    def __init__(self):
+        self.health = 10
+
+    def attack(self, op):
+        pass
+
+
+class ArmoredDron():
+    def __init__(self):
+        self.health = random.randint(10, 13)
+
+    def attack(self, op):
+        op.health -= random.randint(3, 5)
+
+
+class ArmyDron():
+    def __init__(self):
+        self.health = random.randint(8, 9)
+
+    def attack(self, op):
+        op.health -= random.randint(4, 5)
+
+
+class MagicDron():
+    def __init__(self):
+        self.health = random.randint(8, 15)
+
+    def attack(self, op):
+        if self.health > op.health:
+            op.health -= random.randint(6, 8)
+        else:
+            self.health -= 1
+
+
+class Battlefield():
+
+    def play(self, d1, d2):
+        print("First drone - {} health {}\nSecond drone - {} health {}"
+              .format(type(d1).__name__, d1.health, type(d2).__name__,
+                      d2.health))
+        print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
+        while True:
+            d2.attack(d1)
+            print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
+            if d1.health <= 0:
+                print("Second Drone win")
+                break
+            d1.attack(d2)
+            print("Health: 1 - {}, 2 - {}".format(d1.health, d2.health))
+            if d2.health <= 0:
+                print("First Drone win")
+                break
+
+
+field = Battlefield()
+d1 = ArmoredDron()
+d2 = ArmyDron()
+field.play(d1, d2)
